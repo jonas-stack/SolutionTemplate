c7aeb29b691c6d83f7c02ba78e0a02e7c1d256ab
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2024-09-04 19:00:27 +0200

    Backend setup with swagger

--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
+builder.Services.AddOpenApiDocument();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+app.UseOpenApi();
+app.UseSwaggerUi();
+
+app.UseCors(opts => {
+    opts.AllowAnyOrigin();
+    opts.AllowAnyMethod();
+    opts.AllowAnyHeader();
+});
 
 app.Run();
+
+
